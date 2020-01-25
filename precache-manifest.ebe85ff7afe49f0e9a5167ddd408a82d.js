7e4c22a67663501be5785e9106b94ced51cfa80b
joel sithivong <joelsithivong@MacBook-Pro-de-joel.local>
2020-01-25 20:12:09 +0100

    Updates

---
+++ precache-manifest.ebe85ff7afe49f0e9a5167ddd408a82d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a6ce4fae68c0b4a16e15d2da10ccb5",
+    "url": "/App1/index.html"
+  },
+  {
+    "revision": "96cca896ec0ac862cdf3",
+    "url": "/App1/static/css/main.21f4f756.chunk.css"
+  },
+  {
+    "revision": "945b04b3a8136dda3677",
+    "url": "/App1/static/js/2.34ab0423.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/App1/static/js/2.34ab0423.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96cca896ec0ac862cdf3",
+    "url": "/App1/static/js/main.9175c436.chunk.js"
+  },
+  {
+    "revision": "f9a387ccd82d07ec0646",
+    "url": "/App1/static/js/runtime-main.e936345b.js"
+  },
+  {
+    "revision": "837ba7fb0474c0a97743c474be3d87a5",
+    "url": "/App1/static/media/manhattan-336708_1280.837ba7fb.jpg"
+  }
+]);
